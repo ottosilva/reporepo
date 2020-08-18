3cc8f991754211c6c938e2b6f982bfc347f0a8ce
otto <ottosilva12@gmail.com>
2020-08-18 18:45:35 -0300

    añadimos basicos/server react

---
+++ basicos/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hola mundos como anda la bandas
+      </h1>
+    </div>
+  );
+}
+
+export default App;
