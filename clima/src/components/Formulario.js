5a6e4ad9fa3241f86b2fae7c38ae2b85ab340a2c
Otto <ottosilva12@gmail.com>
2020-09-23 17:10:33 -0300

    app consulta de clima, practica API

---
+++ clima/src/components/Formulario.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+
+const Formulario = () => {
+
+    //state del formulario
+    const [busqueda, guardarBusqueda] = useState({
+       ciudad: '',
+       pais: '' 
+    });
+
+    //aplicamos distructuring a busqueda
+    const {ciudad, pais} = busqueda;
+
+    //funcion que coloca los elementos en el state
+    const handleChange = e =>{
+        //actualizar el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+    }
+    return (
+
+        <form>
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
+                    />
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <select
+                    name="pais"
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
+                >
+                    <option value="">--Seleccione un pais--</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>     
+                </select>
+                <label htmlFor="pais">País: </label>        
+            </div>
+                
+           
+        </form>
+    );
+}
+ 
+export default Formulario;
