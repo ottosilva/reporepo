6e6d761d9c2493be780e974041f4fb13e9e1331a
Otto <ottosilva12@gmail.com>
2020-12-15 08:36:54 -0300

    un monton de paquetes, creo q dp de esto se rompe todo

---
+++ presupuesto/build/precache-manifest.53b7d5871bcc094935a4ce5274eb9ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b00f64e68acb3f26c899035763ecc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4429f26232d0bc67bb67",
+    "url": "/static/css/main.c3fcd8c2.chunk.css"
+  },
+  {
+    "revision": "35ff1e67a448da6e77b4",
+    "url": "/static/js/2.ce226546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce226546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4429f26232d0bc67bb67",
+    "url": "/static/js/main.7350db11.chunk.js"
+  },
+  {
+    "revision": "29630ad1ca7e967d236a",
+    "url": "/static/js/runtime-main.ea644902.js"
+  }
+]);
